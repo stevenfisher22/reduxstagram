9b73a1030f298b1917629ada13eacf6990e146ee
Steven Fisher <steven.fisher22@gmail.com>
2019-02-26 21:49:16 -0600

    like button working

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -23,7 +23,7 @@ const Photo = React.createClass({
                 <figcaption>
                     <p>{post.caption}</p>
                     <div className="control-buttons">
-                        <button className="likes">HEART {post.likes}</button>
+                        <button onClick={this.props.increment.bind(null, i)} className="likes">❤️ {post.likes}</button>
                         <Link className="button" to={`/view/${post.code}`}>
                             <span className="comment-count">
                                 <span className="speech-bubble"></span>
